9c4c22fef4df2008d54716180bd0ca333d7c5bdc
Wen Xiao <wennstyle@gmail.com>
2022-02-21 14:52:22 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -12,15 +12,18 @@ function App() {
   }
 
   const displayOrders = orders.map((order, idx) => {
-    <Order key={idx} {...order} />;
+    //needs the key word return if being displayed in an element
+    return <Order key={idx} {...order} />;
   });
+
+  console.log(displayOrders)
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Form />
+      <Form addOrder={addOrder} />
       <div className="ui raised container segment">
         <h1 className="ui block header">All Orders</h1>
         <div className="ui three cards">{displayOrders}</div>
