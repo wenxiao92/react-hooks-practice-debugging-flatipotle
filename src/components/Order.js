d9ee3bb2ff4f448a8265f6f818e4a435ff12857d
Wen Xiao <wennstyle@gmail.com>
2022-02-21 14:52:05 -0500

    commit tree 46d079d8899de08f0d8c57b8743140dda8d30631

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import burritoImg from "../images/burrito-bowl.jpg";
+import Side from "./Side"
 
 function Order(props) {
   const [isClicked, setIsClicked] = useState(false);
 
   function handleClick() {
-    isClicked = !isClicked;
+    setIsClicked((isClicked) => !isClicked);
   }
 
   return (
@@ -37,7 +38,7 @@ function Order(props) {
         )}
 
         {/* this is just a shortcut to writing state.isClicked ? <Side sides={props.sides} /> : null */}
-        {state.isClicked && <Side sides={props.sides} />}
+        {isClicked && <Side sides={props.sides} />}
       </div>
     </div>
   );
